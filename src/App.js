29189ad674159e8a55872a229fc5768157214f1b
Ljubo6 <ljubo6@abv.bg>
2025-01-27 17:30:08 +0200

    init 2

--- src/App.js
+++ src/App.js
@@ -3,52 +3,52 @@ import './App.css';
 import React from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>Current Year:{new Date().getFullYear()}</p>
-			</header>
-		</div>
-	);
-	// return React.createElement(
-	// 	'div',
-	// 	{ className: 'App' },
-	// 	React.createElement('header', { className: 'App-header' }, [
-	// 		React.createElement('img', {
-	// 			src: logo,
-	// 			className: 'App-logo',
-	// 			alt: 'logo',
-	// 		}),
-	// 		React.createElement(
-	// 			'p',
-	// 			null,
-	// 			'Edit ',
-	// 			React.createElement('code', null, 'src/App.js'),
-	// 			' and save to reload.',
-	// 		),
-	// 		React.createElement(
-	// 			'a',
-	// 			{
-	// 				className: 'App-link',
-	// 				href: 'https://reactjs.org',
-	// 				target: '_blank',
-	// 				rel: 'noopener noreferrer',
-	// 			},
-	// 			'Learn React',
-	// 		),
-	// 		React.createElement('p', null, `Current Year: ${new Date().getFullYear()}`),
-	// 	]),
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<p>Current Year:{new Date().getFullYear()}</p>
+	// 		</header>
+	// 	</div>
 	// );
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement('header', { className: 'App-header' }, [
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, `Current Year: ${new Date().getFullYear()}`),
+		]),
+	);
 };
